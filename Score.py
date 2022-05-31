013504cd7a58346c8eef71ef0df0f7b32f3bfba4
JeremyFellous <104926826+JeremyFellous@users.noreply.github.com>
2022-05-31 22:06:36 +0300

    update

--- Score.py
+++ Score.py
@@ -1,8 +1,26 @@
 from Utils import SCORES_FILE_NAME, BAD_RETURN_CODE
+from genericpath import exists
+import os
+
+
+def get_score():
+    exists = os.path.isfile(SCORES_FILE_NAME)
+    if exists:
+        with open(SCORES_FILE_NAME, 'r') as f:
+            return f.read()
+    else:
+        return BAD_RETURN_CODE
 
 
 def add_score(diff):
     point_of_winning = (int(diff) * 3) + 5
-    print(f'\nYour score is: {point_of_winning}')
-
-
+    print(f'\nYour score from this game is: {point_of_winning}')
+    my_score = get_score()
+    if my_score == BAD_RETURN_CODE:
+        with open(SCORES_FILE_NAME, 'w') as f:
+            f.write(str(point_of_winning))
+            print(f'\nYour total score is {point_of_winning}')
+    else:
+        with open(SCORES_FILE_NAME, 'w') as f:
+            f.write(str(point_of_winning + int(my_score)))
+            print(f'\nYour total score is: {point_of_winning + int(my_score)}')
